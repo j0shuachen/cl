979008028a3f75cde7bec82a3f4c70bf7244a502
JC <J0shua.chen@yahoo.com>
2017-10-09 01:40:07 -0700

    update resume

--- frontend/components/info/resume.jsx
+++ frontend/components/info/resume.jsx
@@ -8,10 +8,10 @@ class Resume extends React.Component{
       <div className='resumecontainer'>
         <div className='aboutmaindos'>
           <div className='downicon'>
-        <a href='http://res.cloudinary.com/dxeyfggji/image/upload/v1507112383/ChenJoshResume.pdf'>  <img className='pdfdown' src='http://res.cloudinary.com/dxeyfggji/image/upload/v1506956336/pdf-file-format-symbol_318-45340_plqofm.jpg'/> </a>
-      <a href='http://res.cloudinary.com/dxeyfggji/image/upload/v1507112383/ChenJosh.png' download='ChenJoshuaResume.png' >  <img className='pdfdowndos' src='http://res.cloudinary.com/dxeyfggji/image/upload/v1506956333/download_iy2qpr.png'/></a>
+        <a href='http://res.cloudinary.com/dxeyfggji/image/upload/v1507537842/ChenJoshuaS_d8aken.pdf'>  <img className='pdfdown' src='http://res.cloudinary.com/dxeyfggji/image/upload/v1506956336/pdf-file-format-symbol_318-45340_plqofm.jpg'/> </a>
+      <a href='http://res.cloudinary.com/dxeyfggji/image/upload/v1507537835/ChenJoshuaS_djlvdz.png' download='ChenJoshuaResume.png' >  <img className='pdfdowndos' src='http://res.cloudinary.com/dxeyfggji/image/upload/v1506956333/download_iy2qpr.png'/></a>
           </div>
-        <img className= 'resumeimage' src='http://res.cloudinary.com/dxeyfggji/image/upload/v1507508930/ChenJoshua_t8p29u.png'/>
+        <img className= 'resumeimage' src='http://res.cloudinary.com/dxeyfggji/image/upload/v1507537835/ChenJoshuaS_djlvdz.png'/>
         </div>
       </div>
     );
