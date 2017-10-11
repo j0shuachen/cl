be14aa61379a8eed44e1ca3d40de2cec1c9ff1c6
JC <J0shua.chen@yahoo.com>
2017-10-10 21:18:58 -0700

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,131 @@
+// webpack.config.js
 var path = require("path");
-
 var webpack = require("webpack");
 
 var plugins = []; // if using any plugins for both dev and production
 var devPlugins = []; // if using any plugins for development
 
-// var prodPlugins = [
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       'NODE_ENV': JSON.stringify('production')
-//     }
-//   }),
-//   new webpack.optimize.UglifyJsPlugin({
-//     compress: {
-//       warnings: true
-//     }
-//   })
-// ];
-//
-// plugins = plugins.concat(
-//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-// );
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
 
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
+// include plugins config
 module.exports = {
   context: __dirname,
   entry: "./frontend/meetup.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
   plugins: plugins,
-
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['react', 'es2015']
         }
-      },{
-        test: /\.css$/,
-        loader:  'style-loader!css-loader' 
+      }, {
+                test: /\.css$/,
+                loader:  'style-loader!css-loader'
+
       }
     ]
   },
-  devtool: 'source-maps',
+  devtool: 'source-map',
   resolve: {
     extensions: [".js", ".jsx", "*"]
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var path = require("path");
+//
+// var webpack = require("webpack");
+//
+// var plugins = []; // if using any plugins for both dev and production
+// var devPlugins = []; // if using any plugins for development
+//
+// // var prodPlugins = [
+// //   new webpack.DefinePlugin({
+// //     'process.env': {
+// //       'NODE_ENV': JSON.stringify('production')
+// //     }
+// //   }),
+// //   new webpack.optimize.UglifyJsPlugin({
+// //     compress: {
+// //       warnings: true
+// //     }
+// //   })
+// // ];
+// //
+// // plugins = plugins.concat(
+// //   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// // );
+//
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/meetup.jsx",
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: "bundle.js"
+//   },
+//   plugins: plugins,
+//
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       },{
+//         test: /\.css$/,
+//         loader:  'style-loader!css-loader'
+//       }
+//     ]
+//   },
+//   devtool: 'source-maps',
+//   resolve: {
+//     extensions: [".js", ".jsx", "*"]
+//   }
+// };
